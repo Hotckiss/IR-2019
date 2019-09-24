7682bb10cbff6375ef3a291f9e53734871992e25
Olga Lupuleac <olga.lupuleac@gmail.com>
2019-09-25 00:26:48 +0300

    Add enconding in json converter

--- ir1/json_converter.py
+++ ir1/json_converter.py
@@ -2,13 +2,13 @@ import json
 import os
 from tqdm import tqdm
 
-path_to_text_files = "res/text/"
-path_to_json_files = "res/json/"
+path_to_text_files = "data/filtered_tokens_texts/"
+path_to_json_files = "data/json_filtered_tokens_texts/"
 
 
 def to_json():
     for filename in tqdm(os.listdir(path_to_text_files)):
-        with open(os.path.join(path_to_text_files, filename), "r") as file:
+        with open(os.path.join(path_to_text_files, filename), "r", encoding="utf8") as file:
             content = {
                 "text": file.read()
             }
